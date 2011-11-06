4cd6abb576040b3badbab20864e5c1a341769fd3
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-11-06 09:43:25 +0000

    Updating svn-props

--- xxxxx/class/token.php
+++ xxxxx/class/token.php
@@ -2,7 +2,7 @@
 /**
  *
  * @package Legacy
- * @version $Id: token.php,v 1.3 2008/09/25 15:12:42 kilica Exp $
+ * @version $Id$
  * @copyright Copyright 2005-2007 XOOPS Cube Project  <http://xoopscube.sourceforge.net/>
  * @license http://xoopscube.sourceforge.net/license/GPL_V2.txt GNU GENERAL PUBLIC LICENSE Version 2
  *
